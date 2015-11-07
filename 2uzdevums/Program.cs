0938bfa5f1d119478877ccaa384fb226c6d2786e
Diana Kornejeva <caron@inbox.lv>
2015-11-07 16:18:29 +0200

    2 uzdevums ar konsoles programmu

---
+++ 2uzdevums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2uzdevums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tDiana Kornejeva\n");
+            Console.WriteLine("\t\tInformacijas tehnologijas\n");
+            Console.WriteLine("\t\t3.kurss\n");
+
+            Console.ReadKey();
+        }
+    }
+}
